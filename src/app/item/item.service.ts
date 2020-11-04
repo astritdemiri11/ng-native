0b18f87eee7b77b6a37bcedee1a77c221e5ccb9e
Astrit Demiri <astritdemiri11@gmail.com>
2020-11-04 19:42:48 +0100

    Git Test

--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -9,7 +9,7 @@ export class ItemService {
     private items = new Array<Item>(
         { id: 1, name: "Ter Stegen", role: "Goalkeeper" },
         { id: 3, name: "Piqué", role: "Defender" },
-        { id: 4, name: "I. Rakitic", role: "Midfielder" },
+        { id: 4, name: "Rakitic", role: "Midfielder" },
         { id: 5, name: "Sergio", role: "Midfielder" },
         { id: 6, name: "Denis Suárez", role: "Midfielder" },
         { id: 7, name: "Arda", role: "Midfielder" },
